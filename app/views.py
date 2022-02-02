f88360441d60d7893bb457640d4527ed7e673ff4
feLycks <felix.obiero@student.moringaschool.com>
2022-02-02 20:55:16 +0300

    edit Review build

--- app/views.py
+++ app/views.py
@@ -3,9 +3,13 @@ from app import app
 from .request import get_movies,get_movie
 from .request import get_movies,get_movie,search_movie
 from flask import render_template,request,redirect,url_for
+from .models import review
+from .forms import ReviewForm
 
+Review = review.Review
 
 # Views
+# @TODO: Default page onLoad 
 @app.route('/')
 def index():
 
@@ -27,6 +31,7 @@ def index():
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
+# @TODO: Get | Search movies byID 
 @app.route('/movie/<int:id>')
 def movie(id):
 
@@ -35,9 +40,11 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie)
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
+# @TODO: Search functionality 
 @app.route('/search/<movie_name>')
 def search(movie_name):
     '''
@@ -49,3 +56,20 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     
     return render_template('search.html',movies = searched_movies)
+
+
+# @TODO: Reviews function
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
